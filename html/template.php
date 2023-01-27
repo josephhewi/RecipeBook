d9848d9fe08cbd14a86184466209d598cd53a043
Joe Hewitt <70499731+josephhewi@users.noreply.github.com>
2023-01-26 19:41:42 -0600

    Create template.php

---
+++ html/template.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en-US">
+
+<head>
+  <meta charset="utf-8" />
+  <title></title>
+  <meta name="author" content="Joe Hewitt" />
+  <link rel="stylesheet" href="style.css" />
+</head>
+
+<body>
+
+  <header>
+    <div id="nav"></div>
+  </header>
+  
+  <div id="main"></div>
+  
+  <footer></footer>
+
+</body>
+
+</html>
